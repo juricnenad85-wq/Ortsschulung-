e8f84964527e7f96232d7614443441bffc8ac158
juricnenad85-wq <juricnenad85@gmail.com>
2026-08-19 16:07:02 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ortsschulung-v4-dashboard-fix';
+const CACHE='ortsschulung-v5-status-filter';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{
   self.skipWaiting();
